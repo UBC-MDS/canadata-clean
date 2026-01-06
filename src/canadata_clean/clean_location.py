a4719b1b43d19c201bc16885b4b9711d1d0121b9
Molly Kessler <88510334+EvesK@users.noreply.github.com>
2026-01-06 13:21:03 -0800

    updated documentation for clean_location function

--- src/canadata_clean/clean_location.py
+++ src/canadata_clean/clean_location.py
@@ -1,13 +1,13 @@
 def clean_location(text: str) -> str:
     """
-    Clean and validate a free-text entry representing a location (city and province) in Canada and convert it to the format "City, ProvinceCode". 
+    Clean and validate a free-text entry representing a general location (municipality name and province or territory) in Canada and convert it to the format "MunicipalityName, TwoLetterProvinceOrTerritoryCode". 
     
-    The function accepts locations in a variety of formats. First, it searches for key words and acronyms to identify the specified province from a preset list of province names, including acronyms and shorthands. If a province cannot be identified, the function will throw an error and require the user to add or modify the province before proceeding. Once a province has been identified, the string is modified to remove the characters indicating the province. The modified string is standardized to title case and appropriate white space, and common acronyms and shorthands are identified and standardized using a preset list. Lastly, the city is compared against a list of known cities in Canada. If a city cannot be identified, the function will throw a warning but proceed with the cleaned city name.  
+    The function accepts locations in a variety of formats. First, it searches for key words and acronyms to identify the specified province or territory from a preset list of province and territory names, including acronyms and shorthands. If a province or territory cannot be identified, the function will throw an error and require the user to add or modify the province or territory before proceeding. Once a province or territory has been identified, the string is modified to remove the characters indicating the province or territory. The modified string is standardized to title case and appropriate white space, and common acronyms and shorthands are identified and standardized using a preset list. This string, representing the municipality name, is compared against a list of known municipalities in Canada. If a municipality cannot be identified, the function will throw a warning but proceed with the cleaned municipality name.  
 
     Parameters
     ----------
     text : str
-        The input string representing a location, city and providence, in Canada.
+        The input string representing a location, municipality and providence/territory, in Canada.
 
     Returns
     -------
@@ -17,9 +17,9 @@ def clean_location(text: str) -> str:
     Raises
     ------
     ValueError
-        If a valid Canadian province cannot be identified from the input.
+        If a valid Canadian province/territory cannot be identified from the input.
     Warning
-        If a valid Canadian city cannot be identified from the input.
+        If a valid Canadian municipality cannot be identified from the input.
 
     Examples
     --------
@@ -27,9 +27,8 @@ def clean_location(text: str) -> str:
     'North Vancouver, BC'
     >>> clean_location("Not A City, BC")
     'Not A City BC'
-    # Raises Warning: City could not be identified.
+    # Raises Warning: Municipality could not be identified.
     >>> clean_location("Vancouver BX")
-    # Raises ValueError: Province could not be identified.
-
+    # Raises ValueError: Province or territory could not be identified.
     """
     return str()
